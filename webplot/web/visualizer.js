5b20216049dfe9048c159ff4ce46bb2717f1248a
evgenyslab <e.nuger@gmail.com>
2020-09-02 15:31:27 -0400

    check point with auto-generated html + buttons with functional links

--- webplot/web/visualizer.js
+++ webplot/web/visualizer.js
@@ -2,6 +2,8 @@
 TODO:
 
 - [ ] move all html generation into JS
+- [ ] test ws.message send back
+- [ ] add button for presentation window thru msgpack
 - [ ] create port input at top of page to set WS connection port
 - [ ] bind 'enter' key & button to port input box to attempt connection
 - [ ] add toolbar under WS connection bar with toggle switch/button for Image/Plot/MISC
@@ -28,11 +30,6 @@ resize factor
 
 // msgpack = require("msgpack-lite");
 
-var wsport = "7777";
-
-var ws = new WebSocket("ws://0.0.0.0:" + wsport);
-
-var connected = false;
 
 
 var imageData = {
@@ -40,6 +37,52 @@ var imageData = {
     "height":0,
     "loaded":false
 };
+
+let state = {
+    pageSettings:{
+        tabs:["image","plot","misc"],
+        tabButtons:[
+            {name:"Image",
+                fx:setPageToImage},
+            {name:"Plot",
+                fx:setPageToPlot},
+            {name:"Other",
+                fx:setPageToOther},
+        ]
+    },
+    pageState:{
+        currentView:"image"
+    },
+    wsData:{
+        port: "7777",
+        connected: false,
+        ws: null,
+    },
+    mode: "image",
+    imageData:{
+        "width":0,
+        "height":0,
+        "loaded":false
+    },
+    plotData:{
+
+    }
+
+}
+
+function test(){
+    console.log("ButtonPressed")
+}
+
+function setPageToImage(){
+    console.log("setPageToImage")
+}
+function setPageToPlot(){
+    console.log("setPageToPlot")
+}
+function setPageToOther(){
+    console.log("setPageToOther")
+}
 
 function getCursorPosition(canvas, event) {
     if(imageData.loaded) {
@@ -56,83 +99,19 @@ function clearImage(){
     var context = canvas.getContext('2d');
     context.clearRect(0, 0, canvas.width, canvas.height);
     document.getElementById('imageContainer').style.display = "none";
-
 }
 
-function buildWebPage(){
-    // create divs
-    let divElement = document.createElement("div")
-    divElement.id = "main";
-
-    let divStatus = document.createElement("H1");
-    divStatus.id = "header";
-    let text = document.createTextNode("Visualizer::Disconnected");
-    divStatus.appendChild(text);
-    divElement.appendChild(divStatus);
-
-    document.getElementsByTagName("body")[0].appendChild(divElement);
-
-    // Image Container:
-    let divContainer = document.createElement("div");
-    divContainer.id = "imageContainer";
-    document.getElementsByTagName("body")[0].appendChild(divContainer);
-
-    divContainer = document.createElement("div");
-    divContainer.id = "figureContainer";
-    document.getElementsByTagName("body")[0].appendChild(divContainer);
-
-    // use Js to insert button, otherwise HTML won't link correctly (won't see the onclick defined here)
-    var btn = document.createElement("BUTTON");   // Create a <button> element
-    btn.innerHTML = "Clear Image";
-    btn.onclick = clearImage;
-    document.getElementById("imageContainer").appendChild(btn);
-
-    var cvs = document.createElement("canvas");   // Create a <button> element
-    cvs.id = "viewport";
-    document.getElementById("imageContainer").appendChild(cvs);
-
-    var mcoord = document.createElement("P");   // Create a <button> element
-    mcoord.id = "mouseCoordinates";
-    document.getElementById("imageContainer").appendChild(mcoord);
-
-    // HIDE AFTER LOADING:
-    document.getElementById('imageContainer').style.display = "none";
+function tryToConnectToWS(){
+    state.wsData.ws = new WebSocket("ws://0.0.0.0:" + state.wsData.port);
 }
 
-// Main call function
-(function() {
-
-    buildWebPage()
-    var canvas = document.getElementById('viewport');
-    var context = canvas.getContext('2d');
-
-
-
-    canvas.addEventListener('mousemove', function(e) {
-        getCursorPosition(canvas, e)
-    });
-
-    var plot = document.getElementById('figureContainer')
-    var enablePlotUpdate = true
-
-    plot.addEventListener('mousedown', function(e) {
-        enablePlotUpdate = false
-        console.log("disabling plot updates")
-    });
-
-    plot.addEventListener('mouseup', function(e) {
-        enablePlotUpdate = true
-        console.log("enabling plot updates")
-    });
-
-
-    console.log("Attempting connection");
-    ws.onopen = ()=>{
+function registerWDCallbacks(){
+    state.wsData.ws.onopen = ()=>{
         connected = true;
         document.getElementById("header").innerHTML = 'Visualizer::Connected';
     };
 
-    ws.onmessage = function (event) {
+    state.wsData.ws.onmessage = function (event) {
         var reader = new FileReader();
         // arrow function to retain 'this' & handle byte array conversion + pass to decoder:
         reader.onload = (e) =>{
@@ -159,10 +138,165 @@ function buildWebPage(){
         reader.readAsArrayBuffer(event.data);
     };
 
-    ws.onclose = () =>{
+    state.wsData.ws.onclose = () =>{
         connected = false;
         document.getElementById("header").innerHTML = 'Visualizer::Disconnected';
     };
+}
+
+/*
+
+<div main>
+    <div header>
+    <div buttonToolBar>
+
+<div imageContainer>
+    <div imageButtonToolBar>
+    <div canvasContainer>
+    <div imageInformation>
+
+<div figureContainer>
+    <div figureButtonToolBar>
+    <div canvasContainer>
+    <div imageInformation>
+
+* */
+
+function buildHeader(){
+    // create divs
+    let divMain = document.createElement("div")
+    divMain.id = "main";
+
+    let divHeader = document.createElement("div")
+    divHeader.id = "hero";
+
+    let divHeaderStatus = document.createElement("H1");
+    divHeaderStatus.id = "header";
+    let text = document.createTextNode("Visualizer::Disconnected");
+    divHeaderStatus.appendChild(text);
+    divHeader.appendChild(divHeaderStatus);
+
+
+    let divHeaderButtonToolBar = document.createElement("div")
+    divHeaderButtonToolBar.id = "headerButtonToolBar";
+
+    buttonStyle=
+    "    background-color: #4CAF50;" +
+    "    border: none;" +
+    "    color: white;" +
+    "    text-align: center;" +
+    "    text-decoration: none;" +
+    "    display: inline-block;" +
+    "    font-size: 16px;" +
+    "    margin: 4px 2px;" +
+    "    cursor: pointer;";
+
+    // Auto create buttons:
+    state.pageSettings.tabButtons.forEach((buttonSpec)=>{
+        var btn = document.createElement("BUTTON");   // Create a <button> element
+        btn.innerHTML = buttonSpec.name
+        // btn.onclick = clearImage;
+        btn.onclick = buttonSpec.fx;
+        btn.style.cssText = buttonStyle
+        divHeaderButtonToolBar.appendChild(btn);
+    })
+
+
+    let spacer = document.createElement("br")
+
+    divHeaderButtonToolBar.appendChild(spacer);
+    divHeaderButtonToolBar.appendChild(spacer);
+
+    divMain.appendChild(divHeader)
+    divMain.appendChild(divHeaderButtonToolBar)
+
+    document.getElementsByTagName("body")[0].appendChild(divMain);
+}
+
+function buildImageContainer(){
+    let divImageContainer = document.createElement("div")
+    divImageContainer.id = "imageContainer";
+
+    let divButtonToolBar = document.createElement("div")
+    divButtonToolBar.id = "imageButtonToolBar";
+
+    let divCanvasContainer = document.createElement("div")
+    divCanvasContainer.id = "canvasContainer";
+
+    let divInformationBar = document.createElement("div")
+    divInformationBar.id = "imageInformationBar";
+
+    var btn = document.createElement("BUTTON");   // Create a <button> element
+    btn.innerHTML = "Clear Image";
+    btn.onclick = clearImage;
+    divButtonToolBar.appendChild(btn);
+
+    var canvas = document.createElement("canvas");   // Create a <button> element
+    canvas.id = "viewport";
+    divCanvasContainer.appendChild(canvas);
+
+    var mcoord = document.createElement("P");   // Create a <button> element
+    mcoord.id = "mouseCoordinates";
+    divInformationBar.appendChild(mcoord);
+
+    divImageContainer.appendChild(divButtonToolBar)
+    divImageContainer.appendChild(divCanvasContainer)
+    divImageContainer.appendChild(divInformationBar)
+
+    document.getElementsByTagName("body")[0].appendChild(divImageContainer);
+}
+
+function buildWebPage(){
+    buildHeader()
+    buildImageContainer()
+
+
+    divContainer = document.createElement("div");
+    divContainer.id = "figureContainer";
+    document.getElementsByTagName("body")[0].appendChild(divContainer);
+
+    // use Js to insert button, otherwise HTML won't link correctly (won't see the onclick defined here)
+
+
+
+
+
+
+    // HIDE AFTER LOADING:
+    // document.getElementById('imageContainer').style.display = "none";
+}
+
+// Main call function
+(function() {
+
+    buildWebPage()
+    //  linking functions:
+    var canvas = document.getElementById('viewport');
+    var context = canvas.getContext('2d');
+
+
+    canvas.addEventListener('mousemove', function(e) {
+        getCursorPosition(canvas, e)
+    });
+
+    var plot = document.getElementById('figureContainer')
+    var enablePlotUpdate = true
+
+    plot.addEventListener('mousedown', function(e) {
+        enablePlotUpdate = false
+        console.log("disabling plot updates")
+    });
+
+    plot.addEventListener('mouseup', function(e) {
+        enablePlotUpdate = true
+        console.log("enabling plot updates")
+    });
+
+    // create ws connection:
+    tryToConnectToWS()
+    registerWDCallbacks()
+
+
 
 // register on mouse coordinates over canvas:
 
